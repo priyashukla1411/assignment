ba8d41d1979f30e5ab2c647ac00b2f8e5ad55c9b
priyashukla1411 <100112297+priyashukla1411@users.noreply.github.com>
2022-05-08 18:26:25 +0530

    Update second.sh

--- second.sh
+++ second.sh
@@ -21,7 +21,7 @@ After=network.target
 User=ubuntu
 Group=www-data
 WorkingDirectory=/home/ubuntu/projectdir/projectdir
-ExecStart=/home/ubuntu/projectdir/projectdir/env/bin/gunicorn \
+ExecStart=/home/ubuntu/env/bin/gunicorn \
           --access-logfile - \
           --workers 3 \
           --bind unix:/run/gunicorn.sock \
